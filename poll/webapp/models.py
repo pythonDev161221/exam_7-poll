efa3bc9f3868d6947f44b9bb2ddbd2d103e6f87c
pythonDev161221 <adverbekreklamov@gmail.com>
2022-02-05 11:38:32 +0600

    add models in models.py

--- poll/webapp/models.py
+++ poll/webapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Poll(models.Model):
+    question = models.CharField(max_length=200, verbose_name='Вопрос')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Choice(models.Model):
+    answer = models.CharField(max_length=200)
+    poll = models.ForeignKey('webapp.Poll', related_name='choices',
+                             on_delete=models.CASCADE, verbose_name='Ответы')
