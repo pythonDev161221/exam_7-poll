831c055f9338d2b401905baaabcf0d9bb1550c69
pythonDev161221 <adverbekreklamov@gmail.com>
2022-02-05 11:43:02 +0600

    add site registr in admin.py and add str to models and create superuser

--- poll/webapp/models.py
+++ poll/webapp/models.py
@@ -7,8 +7,15 @@ class Poll(models.Model):
     question = models.CharField(max_length=200, verbose_name='Вопрос')
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f'{self.question}{self.created_at}'
+
 
 class Choice(models.Model):
     answer = models.CharField(max_length=200)
     poll = models.ForeignKey('webapp.Poll', related_name='choices',
                              on_delete=models.CASCADE, verbose_name='Ответы')
+
+    def __str__(self):
+        return f'{self.answer}'
+
