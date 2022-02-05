bf67108794375fdaa62e1dd6ee6c071c469b5cc8
pythonDev161221 <adverbekreklamov@gmail.com>
2022-02-05 11:58:09 +0600

    add base.html

--- poll/webapp/views/polls.py
+++ poll/webapp/views/polls.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView
+
+from webapp.models import Poll
+
+
+class PollListView(ListView):
+    model = Poll
+    template_name = 'polls/poll_list_view.html'
